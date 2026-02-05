17e33c1023f94e9ac02dc4eb8b5f5a32155fdef3
avanderberg48-collab <avanderberg48@gmail.com>
2026-02-05 11:18:16 +0200

    Update android/app/build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.aiapkbuilder.ai_apk_builder_final"
+    namespace = "com.aiapkbuilder.ai_apk_minimal"
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.aiapkbuilder.ai_apk_builder_final"
+        applicationId = "com.aiapkbuilder.ai_apk_minimal"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 31
